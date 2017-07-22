33aa64cf70019559c44261a09682804fd3a9baca
AtomicBrawlers <atomicbrawlers@gmail.com>
2017-07-22 13:38:11 -0400

    -Added some drawer options

--- app/src/main/java/com/atomicbrawlers/arduinodriver/ModeSelect.java
+++ app/src/main/java/com/atomicbrawlers/arduinodriver/ModeSelect.java
@@ -1,5 +1,6 @@
 package com.atomicbrawlers.arduinodriver;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,15 +23,6 @@ public class ModeSelect extends AppCompatActivity
         setContentView(R.layout.activity_mode_select);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +72,13 @@ public class ModeSelect extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        //Example: startActivity(new Intent(this, Auto.class));
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.tab_GTA_drive) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.tab_tank_drive) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.menu_stop) {
 
         }
 
