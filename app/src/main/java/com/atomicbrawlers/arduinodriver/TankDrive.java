c71ceee037ca7dd7cd17afe6cecd544d256b3ff7
AtomicBrawlers <atomicbrawlers@gmail.com>
2017-07-28 21:52:55 -0400

    -Completed TankDrive logic

--- app/src/main/java/com/atomicbrawlers/arduinodriver/TankDrive.java
+++ app/src/main/java/com/atomicbrawlers/arduinodriver/TankDrive.java
@@ -7,22 +7,31 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-//TODO: Connect percentage TextViews to SeekBars
+import static android.R.color.holo_red_dark;
+import static android.R.color.holo_red_light;
+import static com.atomicbrawlers.arduinodriver.R.style.FullscreenTheme;
+
 
 public class TankDrive extends AppCompatActivity {
 
     private View mContentView;
-    private Button mStop, mEmergencyStop;
+    private Button //mStop,
+            mEmergencyStop;
     private SeekBar mLeftDriveInput, mRightDriveInput;
     private TextView mLeftDrivePercentage, mRightDrivePercentage;
 
-    final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
+    private boolean emergencyStop;
+    private int leftInput, rightInput,
+                leftSpeed, rightSpeed;
+
+    private final int RELATIVE_ZERO = 100;
+    private final int THRESH = 30;
+    private final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_FULLSCREEN
             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
     @Override
-    //First thing to run; acts as a constructor
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tank_drive);
@@ -30,11 +39,103 @@ public class TankDrive extends AppCompatActivity {
         mContentView = findViewById(R.id.fullscreen_content_tank);
         mContentView.setSystemUiVisibility(FULLSCREEN);
 
-        mStop                 = (Button)   findViewById(R.id.stop_tank);
+     //   mStop                 = (Button)   findViewById(R.id.stop_tank);
         mEmergencyStop        = (Button)   findViewById(R.id.emergency_stop_tank);
         mLeftDriveInput       = (SeekBar)  findViewById(R.id.left_drive_bar_tank);
         mRightDriveInput      = (SeekBar)  findViewById(R.id.right_drive_bar_tank);
         mLeftDrivePercentage  = (TextView) findViewById(R.id.left_drive_percentage_tank);
         mRightDrivePercentage = (TextView) findViewById(R.id.right_drive_percentage_tank);
+
+        setUpListeners();
+
+        emergencyStop = false;
+        leftInput  = RELATIVE_ZERO;
+        rightInput = RELATIVE_ZERO;
+        leftSpeed  = 0;
+        rightSpeed = 0;
+    }
+
+    private void setUpListeners(){
+        mLeftDriveInput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                leftInput = progress;
+                interpretInput();
+                mLeftDrivePercentage.setText(String.valueOf(leftSpeed) + "%");
+                sendToRobot();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+        });
+
+        mRightDriveInput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rightInput = progress;
+                interpretInput();
+                mRightDrivePercentage.setText(String.valueOf(rightSpeed) + "%");
+                sendToRobot();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+        });
+    }
+
+    public void sendToRobot(){
+        if (!emergencyStop) {
+            //TODO: Send values through Bluetooth here
+        }
+    }
+
+    public void emergencyStop(View view){
+        emergencyStop = true;
+        stop(view);
+
+        mEmergencyStop.setBackgroundColor(getResources().getColor(holo_red_dark,  this.getTheme()));
+        mEmergencyStop.setTextColor      (getResources().getColor(holo_red_light, this.getTheme()));
+        mEmergencyStop.setText           ("EMERGENCY STOPPED");
+    }
+
+    public void stop(View view){
+        leftSpeed  = 0;
+        rightSpeed = 0;
+
+        sendToRobot();
+
+        mLeftDrivePercentage .setText(String.valueOf(leftSpeed)  + "%");
+        mRightDrivePercentage.setText(String.valueOf(rightSpeed) + "%");
+
+        mLeftDriveInput. setProgress(RELATIVE_ZERO);
+        mRightDriveInput.setProgress(RELATIVE_ZERO);
+    }
+
+    public void interpretInput(){ //Converts raw SeekBar values to appropriate motor values
+        if (Math.abs(leftInput - RELATIVE_ZERO) > THRESH){ //Check left input
+            leftSpeed = leftInput - RELATIVE_ZERO;
+        } else {
+            leftSpeed = 0;
+        }
+
+        if (Math.abs(rightInput - RELATIVE_ZERO) > THRESH){ //Check right input
+            rightSpeed = rightInput - RELATIVE_ZERO;
+        } else {
+            rightSpeed = 0;
+        }
     }
 }
