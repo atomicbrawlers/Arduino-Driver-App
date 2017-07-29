4792dda5c63db8de3ae0c9873169865a74b8d9e7
AtomicBrawlers <atomicbrawlers@gmail.com>
2017-07-28 23:54:19 -0400

    -Completed GTADrive logic -GTADrive uses a thread to send data -Renamed Multiple Views in GTADrive

--- app/src/main/java/com/atomicbrawlers/arduinodriver/GTADrive.java
+++ app/src/main/java/com/atomicbrawlers/arduinodriver/GTADrive.java
@@ -7,22 +7,50 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import static android.R.color.holo_red_dark;
+import static android.R.color.holo_red_light;
+
 //TODO: Connect percentage TextViews to SeekBars
 
 public class GTADrive extends AppCompatActivity {
 
     private View mContentView;
-    private Button mStop, mEmergencyStop;
-    private SeekBar mTurningInput, mSpeedInput;
-    private TextView mTurningPercentage, mSpeedPercentage;
+    private Button //mStop,
+                   mEmergencyStop;
+    private SeekBar mSlewInput, mThrottleInput;
+    private TextView mSlewPercentage, mThrottlePercentage;
 
-    final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+    private boolean emergencyStop;
+    private int leftSpeed, rightSpeed,
+                slewInput, throttleInput,
+                slew, throttle;
+
+    private final int RELATIVE_ZERO = 100;
+    private final int THRESH = 30;
+    private final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
+                                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                 | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    class BluetoothTransfer implements Runnable {
+        private volatile boolean exit = false;
+
+        public void run(){
+            while(!exit) { //thread runs inside loop
+                //TODO: Send values through Bluetooth here
+            } //thread stops outside loop
+        }
+
+        public void stop(){
+            exit = true;
+        }
+    }
+
+    private final BluetoothTransfer rDataTransfer= new BluetoothTransfer();
+
+    private final Thread tDataTransfer = new Thread(rDataTransfer);
 
     @Override
-    //First thing to run; acts as a constructor
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gta_drive);
@@ -30,11 +58,124 @@ public class GTADrive extends AppCompatActivity {
         mContentView = findViewById(R.id.fullscreen_content_gta);
         mContentView.setSystemUiVisibility(FULLSCREEN);
 
-        mStop              = (Button)   findViewById(R.id.stop_gta);
-        mEmergencyStop     = (Button)   findViewById(R.id.emergency_stop_gta);
-        mTurningInput      = (SeekBar)  findViewById(R.id.direction_bar_gta);
-        mSpeedInput        = (SeekBar)  findViewById(R.id.drive_bar_gta);
-        mTurningPercentage = (TextView) findViewById(R.id.direction_bar_percentage_gta);
-        mSpeedPercentage   = (TextView) findViewById(R.id.drive_bar_percentage_gta);
+     //   mStop                 = (Button)   findViewById(R.id.stop_gta);
+        mEmergencyStop        = (Button)   findViewById(R.id.emergency_stop_gta);
+        mSlewInput            = (SeekBar)  findViewById(R.id.slew_gta);
+        mThrottleInput        = (SeekBar)  findViewById(R.id.throttle_gta);
+        mSlewPercentage       = (TextView) findViewById(R.id.slew_percentage_gta);
+        mThrottlePercentage   = (TextView) findViewById(R.id.throttle_percentage_gta);
+
+        emergencyStop = false;
+        leftSpeed  = 0;
+        rightSpeed = 0;
+        slewInput     = 0;
+        throttleInput = 0;
+        slew     = 0;
+        throttle = 0;
+
+        setUpListeners();
+        tDataTransfer.start();
+
+    }
+
+    private void setUpListeners(){
+        mSlewInput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                slewInput = progress;
+
+                //Converts raw SeekBar value to a relative one
+                if (Math.abs(slewInput - RELATIVE_ZERO) > THRESH){ //Check slew input
+                    slew = slewInput - RELATIVE_ZERO;
+                } else {
+                    slew = 0;
+                }
+
+                mSlewPercentage.setText(String.valueOf(slew + "%"));
+
+                calculateLogic();
+            }
+
+            @Override
+            public void onStartTrackingTouch (SeekBar seekBar){
+                //Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch (SeekBar seekBar){
+                //Do nothing
+            }
+        });
+
+        mThrottleInput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                throttleInput = progress;
+
+                //Converts raw SeekBar value to a relative one
+                if (Math.abs(throttleInput - RELATIVE_ZERO) > THRESH){ //Check throttle input
+                    throttle = throttleInput - RELATIVE_ZERO;
+                } else {
+                    throttle = 0;
+                }
+
+                mThrottlePercentage.setText(String.valueOf(throttle + "%"));
+
+                calculateLogic();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Do nothing
+            }
+        });
+    }
+
+    private void calculateLogic(){
+        if (!emergencyStop) {
+            if (slew > 0) { //Slew is positive; turning right
+                leftSpeed = throttle;
+                rightSpeed = throttle * (1 - slew); //Send right partial power; based on slew amount
+            } else if (slew < 0) { //Slew is negative; turning left
+                leftSpeed = throttle * (1 + slew); //Send left partial power; based on slew amount
+                rightSpeed = throttle;
+            } else { //No slew; go straight
+                leftSpeed = throttle;
+                rightSpeed = throttle;
+            }
+        }
+    }
+
+    private void sendToRobot(){
+        //TODO: Send values through Bluetooth here
+        //Send left speed
+        //Send right speed
+    }
+
+    public void emergencyStop(View view){
+        emergencyStop = true;
+        rDataTransfer.stop();
+        stop(view);
+
+        mEmergencyStop.setBackgroundColor(getResources().getColor(holo_red_dark,  this.getTheme()));
+        mEmergencyStop.setTextColor      (getResources().getColor(holo_red_light, this.getTheme()));
+        mEmergencyStop.setText           ("EMERGENCY STOPPED");
+    }
+
+    public void stop(View view){
+        throttle = 0;
+
+        sendToRobot();
+
+        mThrottlePercentage.setText(String.valueOf(throttle) + "%");
+
+        mThrottleInput.setProgress(RELATIVE_ZERO);
+
+        System.out.println(tDataTransfer.isAlive());
     }
 }
