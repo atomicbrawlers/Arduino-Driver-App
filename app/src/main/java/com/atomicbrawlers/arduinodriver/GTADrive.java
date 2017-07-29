e568521a902fc42330284bd272d2a7136bda2199
AtomicBrawlers <atomicbrawlers@gmail.com>
2017-07-28 20:21:26 -0400

    -Declared and Initialized Views for GTADrive and TankDrive

--- app/src/main/java/com/atomicbrawlers/arduinodriver/GTADrive.java
+++ app/src/main/java/com/atomicbrawlers/arduinodriver/GTADrive.java
@@ -3,13 +3,18 @@ package com.atomicbrawlers.arduinodriver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-//TODO: Declare and initialize all Views
 //TODO: Connect percentage TextViews to SeekBars
 
 public class GTADrive extends AppCompatActivity {
 
     private View mContentView;
+    private Button mStop, mEmergencyStop;
+    private SeekBar mTurningInput, mSpeedInput;
+    private TextView mTurningPercentage, mSpeedPercentage;
 
     final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -23,7 +28,13 @@ public class GTADrive extends AppCompatActivity {
         setContentView(R.layout.activity_gta_drive);
 
         mContentView = findViewById(R.id.fullscreen_content_gta);
-
         mContentView.setSystemUiVisibility(FULLSCREEN);
+
+        mStop              = (Button)   findViewById(R.id.stop_gta);
+        mEmergencyStop     = (Button)   findViewById(R.id.emergency_stop_gta);
+        mTurningInput      = (SeekBar)  findViewById(R.id.direction_bar_gta);
+        mSpeedInput        = (SeekBar)  findViewById(R.id.drive_bar_gta);
+        mTurningPercentage = (TextView) findViewById(R.id.direction_bar_percentage_gta);
+        mSpeedPercentage   = (TextView) findViewById(R.id.drive_bar_percentage_gta);
     }
 }
--- app/src/main/java/com/atomicbrawlers/arduinodriver/TankDrive.java
+++ app/src/main/java/com/atomicbrawlers/arduinodriver/TankDrive.java
@@ -3,13 +3,18 @@ package com.atomicbrawlers.arduinodriver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-//TODO: Declare and initialize all Views
 //TODO: Connect percentage TextViews to SeekBars
 
 public class TankDrive extends AppCompatActivity {
 
     private View mContentView;
+    private Button mStop, mEmergencyStop;
+    private SeekBar mLeftDriveInput, mRightDriveInput;
+    private TextView mLeftDrivePercentage, mRightDrivePercentage;
 
     final int FULLSCREEN = View.SYSTEM_UI_FLAG_IMMERSIVE
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -23,7 +28,13 @@ public class TankDrive extends AppCompatActivity {
         setContentView(R.layout.activity_tank_drive);
 
         mContentView = findViewById(R.id.fullscreen_content_tank);
-
         mContentView.setSystemUiVisibility(FULLSCREEN);
+
+        mStop                 = (Button)   findViewById(R.id.stop_tank);
+        mEmergencyStop        = (Button)   findViewById(R.id.emergency_stop_tank);
+        mLeftDriveInput       = (SeekBar)  findViewById(R.id.left_drive_bar_tank);
+        mRightDriveInput      = (SeekBar)  findViewById(R.id.right_drive_bar_tank);
+        mLeftDrivePercentage  = (TextView) findViewById(R.id.left_drive_percentage_tank);
+        mRightDrivePercentage = (TextView) findViewById(R.id.right_drive_percentage_tank);
     }
 }
